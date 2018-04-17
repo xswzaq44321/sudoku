081f996f8d26c97200cb50bcb0c345709dc1abec
Weber <xswzaq44321@gmail.com>
2018-04-17 13:12:14 +0800

    typeset Sudoku.h

--- sudoku_class/Sudoku.h
+++ sudoku_class/Sudoku.h
@@ -1,22 +1,21 @@
 class Sudoku
 {
-public:
-    Sudoku();
-    Sudoku(const char initMap[][9]);
-	void setQuiz(int i, int j, char n);
-	char getQuiz(int i, int j);
-	void printQuiz();
-    void takeNote(int a, int b);
-    void clearNote(int a, int b);
-    bool checkDuplicate(int a, int b, int n);
-    int checkSpecial(int a, int b);
-    int check(int a, int b);
-    bool checkUnity(char arr[]);
-    bool isCorrect();
-    void solve();
-private:
-    char quiz[9][9] = {0};
-    char note[9][9][9] = {0};
-    bool initial = true;
+	public:
+		Sudoku();
+		Sudoku(const char initMap[][9]);
+		void setQuiz(int i, int j, char n);
+		char getQuiz(int i, int j);
+		void printQuiz();
+		void takeNote(int a, int b);
+		void clearNote(int a, int b);
+		bool checkDuplicate(int a, int b, int n);
+		int checkSpecial(int a, int b);
+		int check(int a, int b);
+		bool isCorrect();
+		void solve();
+	private:
+		char quiz[9][9] = {0};
+		char note[9][9][9] = {0};
+		bool initial = true;
 };
 
